27e46312521ab92639ff5325dfcc407b4c90317e
sol castro <s.huilencastro@gmail.com>
2021-02-23 16:35:53 -0300

    some animations and cart stuff

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let abrir = document.getElementsByClassName("cta");
 let modal = document.getElementById("modal");
 let modalContainer = document.getElementById("modal-container");
 let modalTotal = document.getElementById("modal-pop");
+let cartIcon = $(".cart-cant");
+let cartLogo = $('#navbar-cart');
+let cartContent = $('#cart-content');
 //cart
 let cart = [];
 
@@ -59,7 +62,7 @@ function displayCards (products){
     products.forEach(product => {
         result += `
         <div id="card-product">
-            <img src="/asset/bauhaus-shop/${product.img}" alt="">
+            <img src="asset/bauhaus-shop/${product.img}" alt="">
             <div>
                 <h3>${product.name}</h3>
                 <p>${product.description}</p>
@@ -73,7 +76,61 @@ function displayCards (products){
 }
 displayCards(listOfProducts);
 
+//display items
 
+
+//funcion para meter en el carrito modal
+//hacer que se guarde el numero del carrito en el storage
+//no se me guarda bien las cosas del storage (creo)
+//meter los items en el carrito-modal bien con los estilos
+
+//display cart modal
+
+$(function() {
+    cartLogo.click(function(event) {
+        event.preventDefault();
+        cartContent.slideToggle(300);
+        console.log("entra");
+    })
+}) 
+
+function showItems(){
+    let cartItem = '';
+    cart.forEach(item => {
+        cartItem += `
+            <div class="cart-flex cart-items">
+                <p>Cant</p>
+                <p>${item.name}</p>
+                <p>${item.price}</p>
+                <span id = ${item.id}x</span>
+            </div>
+        `
+        console.log(cartItem);
+        let afterContent = $("#cart-after")
+        afterContent.after(cartItem);
+        return cartItem;
+        
+    }); 
+}
+
+$(function() {
+    cartLogo.click(function(event) {
+        event.preventDefault();
+        showItems(cart);
+    })
+}) 
+
+
+/* cartLogo.click(function(event){
+    event.preventDefault();
+    cartContent.animate({
+        display: "block",
+        right: "0px"
+    }, console.log("entra")
+    )
+}) */
+
+//cartContent.addClass("change-cart-style")
 
 
 
--- cart-obj.js
+++ cart-obj.js
@@ -12,35 +12,41 @@
    
            cardBtn.forEach(btn =>{
                let id = btn.id;
+               
                let inCart = cart.find(item => item.id == id);
                if(inCart){
                    btn.innerText = "Agregado";
                    btn.disabled = true;
                }
                btn.addEventListener("click", event => {
-                   event.target.innerText = "Agregado";
-                   event.target.disabled = true;
+                    event.target.innerText = "Agregado";
+                    event.target.disabled = true;
    
-                   let cartItem = this.getProductStorage(id);
-                   cart.push(cartItem);
-                   console.log(cartItem);
-                   this.saveCart(cart)
+                    let cartItem = this.getProductStorage(id);
+                    cart.push(cartItem);
+                    console.log(cart);
+                    let cartInCart = cartIcon.html(cart.length);
+                    cartInCart;
+                    this.saveCart(cart);
+                    
                });
            });
        } 
+       //guarda en el storage la lista de todos los productos
        this.saveStorageProducts = function(products) {
            localStorage.setItem('products', JSON.stringify(products));    
        }
+
        this.getProductStorage = function (id){
            let products = JSON.parse(localStorage.getItem("products"));
            let productSeleccionado = products.find(product => product.id == id)
            return productSeleccionado; 
            
        }
+       //guarda en el storage el carrito
        this.saveCart = function (cart) {
            localStorage.setItem("cart", JSON.stringify(cart));
        }
-   
    }
    
    let myCart = new CartFunctions();
